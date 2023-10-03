235163faf45ca41de6e06adf6be0335b172ea5fa
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-10-03 09:10:09 +0300

    unused import

--- src/main/java/com/example/bookstore/model/User.java
+++ src/main/java/com/example/bookstore/model/User.java
@@ -9,7 +9,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
