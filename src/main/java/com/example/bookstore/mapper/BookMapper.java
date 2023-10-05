1783073230500da368e6ad230c8fbefd4f2b4c8d
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-10-05 12:12:55 +0300

    improved Book saving

--- src/main/java/com/example/bookstore/mapper/BookMapper.java
+++ src/main/java/com/example/bookstore/mapper/BookMapper.java
@@ -6,7 +6,6 @@ import com.example.bookstore.dto.book.BookResponseDto;
 import com.example.bookstore.dto.book.CreateBookRequestDto;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.model.Category;
-import java.util.Set;
 import java.util.stream.Collectors;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
@@ -18,7 +17,6 @@ public interface BookMapper {
     BookResponseDto mapToDto(Book book);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "categories", source = "categoryIds")
     Book mapToModel(CreateBookRequestDto dto);
 
     BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
@@ -28,21 +26,5 @@ public interface BookMapper {
         responseDto.setCategoryIds(book.getCategories().stream()
                 .map(Category::getId)
                 .collect(Collectors.toSet()));
-    }
-
-    default Set<Long> mapCategoriesToIds(Set<Category> categories) {
-        return categories.stream()
-                .map(Category::getId)
-                .collect(Collectors.toSet());
-    }
-
-    default Set<Category> mapIdsToCategories(Set<Long> ids) {
-        return ids.stream()
-                .map(id -> {
-                    Category category = new Category();
-                    category.setId(id);
-                    return category;
-                })
-                .collect(Collectors.toSet());
     }
 }
